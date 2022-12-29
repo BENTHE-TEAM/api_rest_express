ea0d9be07955b8955ea7458e285226cfb541fea6
benthebagnan <benthebagnan@gmail.com>
2022-12-29 07:37:28 +0000

    first commit

---
+++ db.config.js
@@ -0,0 +1,17 @@
+// Imprt des module necessaire
+const { Sequelize } = require("sequelize");
+
+// Connexion a la base de donnees
+let sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,29 @@
+// Import modules
+const express = require("express");
+const cors = require("cors");
+
+let DB = require("./db.config");
+
+// Initialisation de l'API
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Mise en place du routing
+app.get("/", (req, res) => res.send("Welcome to my site"));
+app.get("*", (req, res) => res.status(501).send("Error 501"));
+
+//Start server with DB
+
+DB.authenticate()
+  .then(() => console.log("Connected"))
+  .then(() => {
+    app.listen(process.env.SERVER_PORT, () => {
+      console.log(
+        `This dddd server is rouning on port ${process.env.SERVER_PORT}`
+      );
+    });
+  })
+  .catch((error) => console.log(`Error ${error}`));
