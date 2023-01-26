3a7188b92c5c1a59fc5a3bcff4eb12c5b1dd1583
benthebagnan <benthebagnan@gmail.com>
2023-01-26 18:30:06 +0000

    Add async await

--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ module.exports = (sequelize) => {
       },
     },
     { paranoid: true }
-  ); // Ici pour faire du softDelete
+  );
 
   User.beforeCreate(async (user, options) => {
     let hash = await bcrypt.hash(
@@ -53,41 +53,3 @@ module.exports = (sequelize) => {
 
   return User;
 };
-
-// const { Sequelize, DataTypes } = require("sequelize");
-// const DB = require("../db.config");
-
-// const User = DB.define(
-//   "User",
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       autoIncrement: true,
-//       primaryKey: true,
-//     },
-//     firstName: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     lastName: {
-//       type: DataTypes.STRING,
-//     },
-//     pseudo: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       validate: {
-//         isEmail: true,
-//       },
-//     },
-//     password: {
-//       type: DataTypes.STRING(64),
-//       is: /^[0-9a-f]{64}$/i,
-//     },
-//   },
-//   { paranoid: true }
-// );
-
-// module.exports = User;
